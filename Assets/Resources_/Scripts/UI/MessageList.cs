f19a7a48843ec830787b2dbca0125d7150ba1ffb
hurenn <suzukihuren070@gmail.com>
2025-09-17 08:39:14 +0900

    メッセージ管理初期実装

---
+++ Assets/Resources_/Scripts/UI/MessageList.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MessageData {
+    public string text;   // メッセージ
+    public Sprite characterIcon;    // キャラクターアイコン
+    public string characterName;    // キャラクター名
+    public bool isForced;         // 強制メッセージかどうか
+    public bool isSeries;         // 連続メッセージかどうか
+    public bool isSeriesEnd;      // 連続メッセージの最終メッセージかどうか
+
+    public MessageData(string text, Sprite characterIcon, string characterName, bool isForced, bool isSeries, bool isSeriesEnd) {
+        this.text = text;
+        this.characterIcon = characterIcon;
+        this.characterName = characterName;
+        this.isForced = isForced;
+        this.isSeries = isSeries;
+        this.isSeriesEnd = isSeriesEnd;
+    }
+}
+
+/// <summary>
+/// メッセージリスト管理クラス
+/// </summary>
+public class MessageList : MonoBehaviour {
+    public static MessageList Instance { get; private set; }
+
+    // メッセージキュー
+    private Queue<MessageData> _messageQueue = new Queue<MessageData>();
+
+    // メッセージを登録する
+    public void Enqueue(MessageData messageData) => _messageQueue.Enqueue(messageData);
+    // メッセージを取得して削除する
+    public MessageData Dequeue() => _messageQueue.Dequeue();
+
+    // 表示待ちメッセージがあるか確認する
+    public bool HasMessages() {
+        return _messageQueue.Count > 0;
+    }
+
+    //private bool _isForceMessageViewing;    // 強制メッセージが流れているか判定
+    //public bool isForceMessageViewing {
+    //    get { return _isForceMessageViewing; }
+    //    set { _isForceMessageViewing = value; }
+    //}
+    //private bool _isMessageViewing;         // なんらかのメッセージが表示中か判定
+    //public bool isMessageViewing {
+    //    get { return _isMessageViewing; }
+    //    set { _isMessageViewing = value; }
+    //}
+
+    //private void _Reset() {
+    //    _isMessageViewing = false;
+    //    GameObject.Find("Message Window").transform.GetChild(0).gameObject.SetActive(false);
+    //}
+
+    // Update is called once per frame
+    //void Update() {
+        ////強制メッセージが流れたとき
+        //if (_isForceMessageViewing) {
+        //    gameObject.SetActive(false);
+        //    _Reset();
+        //    return;
+        //}
+
+        //if (!_isMessageViewing)//現在進行中の他メッセージがあるかをチェック
+        //{
+        //    _messageCount = this.transform.childCount;  //メッセージの数を取得
+        //    _activate();
+        //}
+    //}
+
+    //IEnumerator _ViewMessageWait() {
+    //    _isMessageViewing = true;   //メッセージ進行中フラグ設定
+    //    _currentMessage++;          //次のメッセージへ移行
+    //    transform.GetChild(_currentMessage).gameObject.SetActive(true); //順番にメッセージ表示
+
+    //    yield return new WaitForSeconds(1f);
+    //    _activate();         //残りメッセージ判定
+    //}
+
+    //private void _activate() {
+    //    if (_currentMessage < _messageCount)  // 全メッセージ表示までループ
+    //    {
+    //        StartCoroutine(_ViewMessageWait());
+    //    }
+    //}
+}
---
+++ Assets/Resources_/Scripts/UI/MessageTrigger.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using UnityEditor.ShaderKeywordFilter;
+using UnityEngine;
+
+/// <summary>
+/// メッセージを表示させるトリガークラス
+/// </summary>
+public class MessageTrigger : MonoBehaviour {
+    [SerializeField] private MessageList _messageListScript; // メッセージリスト管理
+    [SerializeField] private MessageData[] _messageDatas;  //メッセージデータ配列
+
+    // メッセージ表示待機フラグ(他のメッセージ表示中にトリガーオブジェクトを抜けた時、メッセージ表示を行わないようにするためのフラグ)
+    private bool _messageAddWait = false;
+
+    // メッセージを追加する
+    private void _AddMessage() {
+        foreach (MessageData message in _messageDatas) {
+            _messageListScript.Enqueue(message);
+        }
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            _AddMessage();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            //_messageAddWait = false;  //プレイヤーが離れた時（他のメッセージ表示中に、このトリガーオブジェクトを抜けた時）開始フラグを戻す
+        }
+    }
+
+    //IEnumerator endMessage() {
+    //    _messageListScript.isForceMessageViewing = true;
+    //    yield return new WaitForSeconds(0.2f);
+    //    _messageListScript.isForceMessageViewing = false;
+    //    _MessageStart();
+    //}
+
+    //// Update is called once per frame
+    //void Update() {
+    //    if (_messageListScript.isForceMessageViewing) {
+    //        _messageStartWait = false;
+    //    }
+
+    //    if (_messageStartWait == true)  //メッセージ開始フラグが設定されているとき
+    //    {
+    //        if (!_messageListScript.isMessageViewing)  //かつ、他のメッセージが表示されていないとき
+    //        {
+    //            _MessageStart();
+    //        }
+    //    }
+    //}
+
+    ////メッセージ開始処理
+    //private void _MessageStart() {
+    //    messageList.SetActive(true);  //メッセージリストをアクティブ化
+
+    //    gameObject.SetActive(false);
+    //    //Destroy(gameObject, 1f);  //このトリガーオブジェクトを消す
+    //}
+}
