4a7327c84be1d6d7c08b1dd73097993484757b6a
hurenn <suzukihuren070@gmail.com>
2026-01-30 08:15:46 +0900

    能力確認画面追加

--- Assets/Resources_/Scripts/UI/Pause/Pause_AbilityMenu.cs
+++ Assets/Resources_/Scripts/UI/Pause/Pause_AbilityMenu.cs
@@ -1,15 +1,74 @@
 using UnityEngine;
 
 public class Pause_AbilityMenu : Pause_MenuBase {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    [SerializeField] private RectTransform _buttonY;
+    [SerializeField] private RectTransform _buttonX;
+    [SerializeField] private RectTransform _buttonB;
+    [SerializeField] private RectTransform _buttonA;
+
+    private enum eButtonIndex {
+        Y = 0,
+        X = 1,
+        B = 2,
+        A = 3,
+    }
+    private eButtonIndex _currentButton = eButtonIndex.B;
+
+    private enum eInputDirection {
+        None = 0,
+        Up = 1,
+        Down = 2,
+        Right = 3,
+        Left = 4,
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public override void OnInputVertical(int dir) {
+        eInputDirection dir_enum = dir > 0 ? eInputDirection.Up : eInputDirection.Down;
+        SelectButton(dir_enum);
+    }
+
+    public override void OnInputHorizontal(int dir) {
+        eInputDirection dir_enum = dir > 0 ? eInputDirection.Right : eInputDirection.Left;
+        SelectButton(dir_enum);
+    }
+
+    // PlayerController‚©‚çŒÄ‚Î‚ê‚é•ûŒü“ü—Íˆ—
+    private void SelectButton(eInputDirection dir) {
+
+        switch (dir) {
+            case eInputDirection.Up:
+                _currentButton = eButtonIndex.X;
+                break;
+            case eInputDirection.Down:
+                _currentButton = eButtonIndex.B;
+                break;
+            case eInputDirection.Left:
+                if (_currentButton == eButtonIndex.Y) {
+                    OnSwitchMenu(-1);
+                } else {
+                    _currentButton = eButtonIndex.Y;
+                }
+                break;
+            case eInputDirection.Right:
+                if (_currentButton == eButtonIndex.A) {
+                    OnSwitchMenu(1);
+                } else {
+                    _currentButton = eButtonIndex.A;
+                }
+                break;
+        }
+
+        // ‘I‘ğ‰¹‚ğÄ¶
+        if (_audioSource != null && _seSelect != null) {
+            _audioSource.PlayOneShot(_seSelect);
+        }
+    }
+
+    public override void ExecuteSelectedMenu() {
+        base.ExecuteSelectedMenu();
+        // Œˆ’è‰¹‚ğÄ¶
+        if (_audioSource != null && _seDecide != null) {
+            _audioSource.PlayOneShot(_seDecide);
+        }
     }
 }
