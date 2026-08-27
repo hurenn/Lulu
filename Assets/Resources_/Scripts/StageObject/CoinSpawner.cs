cd2a623ea11976e1058ec5d1789b249611a18f2c
hurenn <suzukihuren070@gmail.com>
2026-08-28 03:26:02 +0900

    コインの端数の消失を防止

--- Assets/Resources_/Scripts/StageObject/CoinSpawner.cs
+++ Assets/Resources_/Scripts/StageObject/CoinSpawner.cs
@@ -17,11 +17,13 @@ public class CoinSpawner : MonoBehaviour {
     private IEnumerator SpawnCoinCoroutine(int value, float interval) {
         int coin_value = 1;
         int coin_count = value;
+        int remainder = 0;
 
         // 大量のコインを一度に生成する場合、価値の高いコインに変換して生成数を減らす
         if (value > 50) {
             coin_value = 5;
             coin_count = value / coin_value;
+            remainder = value % coin_value; // 割り切れない端数（整数除算による価値の消失を防ぐ）
         }
 
         var coin_pool = CoinPool.Instance;
@@ -30,6 +32,13 @@ public class CoinSpawner : MonoBehaviour {
             var coin = coin_pool.SpawnCoin(spawnPosition);
             coin.SetCoinValue(coin_value);
             yield return new WaitForSeconds(interval);
+        }
+
+        // 端数分を1枚のコインとして生成し、価値が静かに減るのを防ぐ
+        if (remainder > 0) {
+            Vector3 spawnPosition = transform.position + new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), 0);
+            var coin = coin_pool.SpawnCoin(spawnPosition);
+            coin.SetCoinValue(remainder);
         }
     }
 }
